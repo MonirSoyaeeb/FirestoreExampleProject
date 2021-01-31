612086c7d1b3687a16b3c1ceadfeed019913e06e
Soyaeeb <monirul15-1742@diu.edu.bd>
2021-01-31 13:35:44 +0600

    Commit 04

--- app/src/main/java/com/example/firestoreexampleproject/MainActivity.java
+++ app/src/main/java/com/example/firestoreexampleproject/MainActivity.java
@@ -1,43 +1,18 @@
 package com.example.firestoreexampleproject;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.ListenerRegistration;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.SetOptions;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-
-    private static final String KEY_TITLE = "title";
-    private static final String KEY_DESCRIPTION = "description";
 
     private EditText editTextTitle;
     private EditText editTextDescription;
@@ -47,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference notebookRef = db.collection("Notebook");
-    private DocumentReference noteRef = db.collection("Notebook").document("My first note");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,63 +47,24 @@ public class MainActivity extends AppCompatActivity {
         notebookRef.add(note);
     }
 
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        notebookRef.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                String data = "";
-                if (error != null) {
-                    return;
-                } else {
-                    for (QueryDocumentSnapshot documentSnapshot : value) {
-                        Note note = documentSnapshot.toObject(Note.class);
-                        note.setDocumentID(documentSnapshot.getId());
-
-                        String documentID = note.getDocumentID();
-                        String title = note.getTitle();
-                        String description = note.getDescription();
-                        int priority = note.getPriority();
-
-                        data += "DocumentId :" + documentID + "\nTitle :" + title + "\n" + "Description: " + description + "\nPriority : " + priority + "\n\n";
-
-                    }
-                    textViewData.setText(data);
-                }
-            }
-        });
-
-    }
-
     public void loadNotes(View view) {
-        Task task1 = notebookRef.whereLessThan("priority", 2).orderBy("priority").get();
-        Task task2 = notebookRef.whereGreaterThan("priority", 2).orderBy("priority").get();
+       notebookRef.orderBy("priority").startAt(3).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+           @Override
+           public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+               String data = "";
+               for(QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                   Note note = documentSnapshot.toObject(Note.class);
+                   note.setDocumentID(documentSnapshot.getId());
 
-        Task<List<QuerySnapshot>> allTasks = Tasks.whenAllSuccess(task1, task2);
+                   String documentId = note.getDocumentID();
+                   String title = note.getTitle();
+                   String description = note.getDescription();
+                   int priority = note.getPriority();
 
-        allTasks.addOnSuccessListener(new OnSuccessListener<List<QuerySnapshot>>() {
-            @Override
-            public void onSuccess(List<QuerySnapshot> querySnapshots) {
-                String data = "";
-                for (QuerySnapshot queryDocumentSnapshots : querySnapshots) {
-                    for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
-                        Note note = documentSnapshot.toObject(Note.class);
-                        note.setDocumentID(documentSnapshot.getId());
-
-                        String documentID = note.getDocumentID();
-                        String title = note.getTitle();
-                        String description = note.getDescription();
-                        int priority = note.getPriority();
-
-                        data += "DocumentId :" + documentID + "\nTitle :" + title + "\n" + "Description: " + description + "\nPriority : " + priority + "\n\n";
-
-                    }
-                    textViewData.setText(data);
-                }
-            }
-        });
-
+                   data+= "ID"+documentId+"\nTitle"+title+"\nDescription"+description+"\nPriority"+priority+"\n\n";
+               }
+               textViewData.setText(data);
+           }
+       });
     }
 }
